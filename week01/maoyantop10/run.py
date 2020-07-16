42eb9c3b97c80cef37ab4f10a0a8379277b17b97
WMWei <wm1993.wei@gmail.com>
2020-07-16 21:35:57 +0800

    update week01 maoyantop10 run.py

--- week01/maoyantop10/run.py
+++ week01/maoyantop10/run.py
@@ -2,7 +2,7 @@ import os
 import sys
 from scrapy import cmdline
 
-base_dir = os.path.dirname(os.path.abspath('__file__'))  # 项目根目录
+base_dir = os.path.dirname(os.path.abspath(__file__))  # 项目根目录
 sys.path.append(base_dir)  # 加入环境变量
 os.chdir(base_dir)  # 切换工作目录
 cmdline.execute(['scrapy', 'crawl', 'maoyanspider'])
