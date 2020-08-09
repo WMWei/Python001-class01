dd3fa4c05ee206a1473680bcfe23966c13d4ef94
WMWei <wm1993.wei@gmail.com>
2020-08-09 23:52:54 +0800

    week07 作业

--- week07/zoo.py
+++ week07/zoo.py
@@ -4,106 +4,76 @@ from abc import ABC, abstractmethod
 # 动物类
 # 抽象基类
 class Animal(ABC):
-    Category = (
-        '食草',
-        '食肉',
-        '杂食',
-    )
-    Size = {
-        '大': 3,
-        '中': 2,
-        '小': 1,
-    }
-    Character = (
-        '温顺',
-        '凶猛',
-    )
 
     def __init__(self,
-                 category,
-                 size,
-                 character):
+                 name: str,
+                 category: str,
+                 size: str,
+                 character: str):
+        self.name = name
         self.category = category
-        self.size = __class__.Size[size]
+        self.size = size
         self.character = character
     
     @property
-    def isviolent(self):
+    def is_violent(self) -> bool:
         '''判断是否是凶猛动物'''
-        return self.size >= __class__.Size['中'] and self.character == '凶猛'
+        return (self.size != '小' and 
+                self.character == '凶猛' and 
+                self.category == '食肉')
+    
+    def __str__(self):
+        return f'{self.__class__.__name__}: {self.name}'
+    
+    def __repr__(self):
+        return (f'{self.__class__.__name__!r}'
+                f'({self.name!r},'
+                f'{self.category!r},'
+                f'{self.size!r},'
+                f'{self.character!r})')
 
-    @abstractmethod
-    def __eq__(self, other):
-        '''用于动物及其子类实例之间的比较'''
 
 
 class Cat(Animal):
-    Voice = None
+    Voice = '喵~'
 
-    def __init__(self,
-                 name,
-                 category,
-                 size,
-                 character):
-        super(Cat, self).__init__(category,
-                                  size,
-                                  character)
-        self.name = name
-
-    def __eq__(self, other):
-        '''判断是否是同一只猫'''
-        return (self.name == other.name and
-                self.category == other.category and
-                self.size == other.size and
-                self.character == other.character)
-    
-    def __str__(self):
-        return self.name
-    
-    __repr__ = __str__
-    
     @property
-    def ispet(self):
+    def as_pet(self) -> bool:
+        '''判断是否适合作为宠物'''
         return not self.isviolent
 
 
 class Zoo(object):
     def __init__(self, name):
         self.name = name
-        self.__animals = {}
+        self.__animals = set()
 
-    @property
-    def animals(self):
-        return self.__animals
-    
     def add_animal(self, animal: 'Animal'):
-        cur_animals = self.__animals.get(animal.__class__.__name__, None)
-        if cur_animals is None:
-            self.__animals[animal.__class__.__name__] = [animal]
-            print(f'成功添加{animal.name}')
+        if animal not in self.__animals:
+            self.__animals.add(animal)
+            print(f'添加{animal}到"{self.name}"成功')
         else:
-            if animal not in cur_animals:
-                cur_animals.append(animal)
-                print(f'成功添加{animal.name}')
-            else:
-                print(f'{animal.name}已存在')
+            print(f'{animal}已经存在于"{self.name}"中')
     
-    def have_animal(self, animal_type):
-        have_ani = self.__animals.get(animal_type, None)
-        if have_ani:
-            return True
-        return False
-
-
+    def __getattr__(self, class_: str) -> bool:
+        return any(class_ == animal.__class__.__name__
+                   for animal in self.__animals)
+    
+    def __str__(self):
+        return f'动物园：{self.name}\n动物：{self.__animals}'
 
 
 if __name__ == '__main__':
     # 实例化动物园
     z = Zoo('时间动物园')
     # 实例化一只猫，属性包括名字、类型、体型、性格
-    cat1 = Cat('大花猫 1', '食肉', '小', '温顺')
+    cat1 = Cat('大花猫', '食肉', '小', '温顺')
+    cat2 = Cat('大白猫', '食肉', '大', '凶猛')
+
     # 增加一只猫到动物园
     z.add_animal(cat1)
-    print(z.animals)
-    # 动物园是否有猫这种动物
-    print(z.have_animal('Cat'))
+    z.add_animal(cat1)
+    z.add_animal(cat2)
+
+    print(z)
+    print(getattr(z, 'Cat'))
