04314cfab853bfce86d588acc4061d6bbd8f492d
WMWei <wm1993.wei@gmail.com>
2020-08-24 07:49:23 +0800

    week09 通过bootstrap渲染

--- week09/task09/myapp/urls.py
+++ week09/task09/myapp/urls.py
@@ -8,6 +8,10 @@ app_name = 'myapp'
 urlpatterns = [
     # localhost:8000/
     path('', views.home, name='home'),
-    # localhost:8000/login
-    path('login/', views.login_view, name='login')
+    # localhost:8000/login/
+    path('login/', views.login_view, name='login'),
+    # # localhost:8000/register/
+    # path('register/', views.register, name='register'),
+    # # localhost:8000/logout/
+    # path('logout/', views.logout, name='logout'),
 ]
--- week09/task09/myapp/views.py
+++ week09/task09/myapp/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 
 
 from .form import LoginForm
@@ -13,27 +14,37 @@ def home(request):
     return render(request, 'home.html')
 
 
+@require_http_methods(['GET', 'POST'])
 def login_view(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             login_info = form.cleaned_data
-            is_user = authenticate(
+            user = authenticate(
                 username=login_info['username'],
                 password=login_info['password'],
             )
-            if is_user:
-                login(request, is_user)
+            if user:
+                login(request, user)
                 messages.success(request, '登录成功')
-                return redirect(f'{reverse("myapp:home")}')
+                return redirect(f'{reverse("myapp:home")}', username=login_info['username'])
             else:
                 messages.error(request, '用户名或者密码错误')
                 return redirect(f'{reverse("myapp:login")}')
     # 如果是get请求，返回登录的表单页面
-    if request.method == 'GET':
+    else:
         form = LoginForm()
         return render(
             request,
             'login.html',
             locals(),
         )
+
+
+# def register(request):
+#     pass
+#     return render(request,'register.html')
+
+# def logout(request):
+#     pass
+#     return redirect('/index/')
